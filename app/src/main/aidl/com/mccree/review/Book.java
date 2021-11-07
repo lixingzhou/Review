c1ad7783e46dc7489bd514499e921e58e25d4890
lixingzhou <lxz737146626@163.com>
2021-11-07 12:35:17 +0800

    本地新分支提交修改

---
+++ app/src/main/aidl/com/mccree/review/Book.java
@@ -0,0 +1,47 @@
+package com.mccree.review.module.aldl;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by: lixingzhou
+ * Created Date: 2021/10/22 12:00
+ * Description:Android开发艺术探索 IPC-Binder
+ */
+public class Book implements Parcelable {
+    public int bookId;
+    public String bookName;
+
+    public Book(int bookId, String bookName) {
+        this.bookId = bookId;
+        this.bookName = bookName;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel out, int flags) {
+        out.writeInt(bookId);
+        out.writeString(bookName);
+    }
+
+    public static final Creator<Book> CREATOR = new Creator<Book>() {
+        @Override
+        public Book createFromParcel(Parcel in) {
+            return new Book(in);
+        }
+
+        @Override
+        public Book[] newArray(int size) {
+            return new Book[size];
+        }
+    };
+
+    protected Book(Parcel in) {
+        bookId = in.readInt();
+        bookName = in.readString();
+    }
+}
